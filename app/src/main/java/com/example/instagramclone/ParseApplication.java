46c9561a78348204661147079b2263a19bc8d884
GuyL <lloydlingani@gmail.com>
2020-10-25 23:06:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    //Initialize Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Register your parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ZECStDcTawEqXw6PxEb4xAcIicHxmUPLpyNJNO18")
+                .clientKey("2DbpGU35YsvW692aXjk18Bo9fpD91QlbqFd7T8Gs")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
